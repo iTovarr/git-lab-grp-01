dd43b740c419b6bb992fe571abba832c058fe0ac
angelo-380 <angelo.ricasca@tecsup.edu.pe>
2026-03-28 13:44:29 -0500

    feat: se agrega StudentView en la carpeta src

---
+++ src/StudentView.java
@@ -0,0 +1,8 @@
+public class StudentView {
+
+    public void displayStudent(String fullName) {
+        System.out.println("===== VISTA DEL ESTUDIANTE =====");
+        System.out.println("Estudiante: " + fullName);
+        System.out.println("=================================");
+    }
+}
