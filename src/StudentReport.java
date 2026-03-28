2832a082397f86451d4e8de678fb70fbb3c2c82a
Sebass007 <fortsad156@gmail.com>
2026-03-28 13:41:45 -0500

    feat: se agrega StudentReport en la carpeta src

---
+++ src/StudentReport.java
@@ -0,0 +1,8 @@
+public class StudentReport {
+    
+    public void generateReport() {
+        System.out.println("--- GENERANDO REPORTE DE ESTUDIANTES ---");
+        System.out.println("Estado: Procesando datos...");
+        System.out.println("Reporte generado con éxito el día de hoy.");
+    }
+}
