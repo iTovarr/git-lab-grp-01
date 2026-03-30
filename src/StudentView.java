dcaaab5109d6bed12dc46d40f9a8777b4a151032
angelo-380 <angelo.ricasca@tecsup.edu.pe>
2026-03-29 22:58:23 -0500

    feat(sprint-2): mejora en StudentView.java — documentacion agregada

--- src/StudentView.java
+++ src/StudentView.java
@@ -1,5 +1,5 @@
 public class StudentView {
-    //public void
+    //esta es la vista
     public void displayStudent(String fullName) {
         System.out.println("===== VISTA DEL ESTUDIANTE =====");
         System.out.println("Estudiante: " + fullName);
