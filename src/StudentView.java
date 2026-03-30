4421983f7a2100b31933832e3c399d3012a6e8bd
angelo-380 <angelo.ricasca@tecsup.edu.pe>
2026-03-29 22:29:39 -0500

    feat(sprint-1): implementacion de StudentView.java

--- src/StudentView.java
+++ src/StudentView.java
@@ -1,5 +1,5 @@
 public class StudentView {
-
+    //public void
     public void displayStudent(String fullName) {
         System.out.println("===== VISTA DEL ESTUDIANTE =====");
         System.out.println("Estudiante: " + fullName);
